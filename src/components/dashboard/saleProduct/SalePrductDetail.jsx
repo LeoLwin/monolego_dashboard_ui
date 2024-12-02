7cb5605a7c5cbb8eaca8869649ad73835b05ec66
KHL <phoekaung.3819@gmail.com>
2024-12-02 07:52:26 +0630

    Update

--- src/components/dashboard/saleProduct/SalePrductDetail.jsx
+++ src/components/dashboard/saleProduct/SalePrductDetail.jsx
@@ -5,8 +5,8 @@ export const SalePrductDetail = ({ data, onClose }) => {
       <div className="absolute inset-0 bg-gray-900 bg-opacity-50 flex items-center justify-center z-50 h-screen">
         <div className="bg-white p-6 rounded-lg shadow-lg w-1/2">
           <h2 className="text-2xl font-bold mb-4">Transaction Details</h2>
-          {data ? (
-            <div>
+          {/* {data ? (
+            <div className="">
               {Object.entries(data).map(([key, value]) => (
                 <p key={key} className="mb-2">
                   <strong>{key.replace(/_/g, " ")}:</strong> {value || "0"}
@@ -15,7 +15,7 @@ export const SalePrductDetail = ({ data, onClose }) => {
             </div>
           ) : (
             <p>No details available.</p>
-          )}
+          )} */}
           <button
             onClick={onClose}
             className="mt-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"

0e4337936ad949d6bb73c9b031c91b9db79e2955
KHL <phoekaung.3819@gmail.com>
2024-12-19 21:05:04 +0630

    Update of order

--- src/components/dashboard/order/Order.jsx
+++ src/components/dashboard/order/Order.jsx
@@ -1,11 +1,102 @@
 import { useState } from "react";
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
+import axios from "axios";
+import { useAuth } from "../../../AuthContext";
 
 /* eslint-disable react/prop-types */
 const Order = ({ data, onClose }) => {
-  // eslint-disable-next-line no-unused-vars
-  const [promoPercentage, setPromoPercentage] = useState(null);
+  const { accessToken } = useAuth();
+  const [showError, setShowError] = useState("");
+  const [code, setCode] = useState("");
+  const [showModal, setShowModal] = useState(false);
+  const [orderData, setOrderData] = useState({
+    id: "",
+    qty: "",
+    promoPercentage: "",
+  });
 
-  console.log("Data : ", data);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    if (name === "qty" && value < 0) return; // Prevent negative values
+    setOrderData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleDoneClick = () => {
+    setShowModal(true); // Open the modal
+  };
+  const saveOrder = async () => {
+    try {
+      // if (!areAllFieldsValid()) {
+      //   setShowError("All fields must be filled out and valid.");
+      //   return;
+      // }
+
+      const userConfirmed = window.confirm(
+        "Are you sure you want to place this order?"
+      );
+      if (!userConfirmed) {
+        return; // Exit if the user cancels
+      }
+      const serverDomain = import.meta.env.VITE_SERVER_DOMAIN;
+      let result;
+
+      result = await axios.post(
+        `${serverDomain}/lego/order/order`,
+        {
+          id: "",
+          qty: "",
+          promoPercentage: "",
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+
+      console.log("Result :", result.data);
+      console.log("Result :", result.data.code);
+      if (result.data.code == "200") {
+        setCode("green");
+      } else {
+        setCode("red");
+      }
+      setShowError(result.data.message);
+
+      // editAble = false;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const downloadAsImage = () => {
+    const element = document.getElementById("voucher-div");
+    html2canvas(element).then((canvas) => {
+      const image = canvas.toDataURL("image/png");
+      const link = document.createElement("a");
+      link.href = image;
+      link.download = "voucher.png";
+      link.click();
+    });
+  };
+
+  const downloadAsPDF = () => {
+    const element = document.getElementById("voucher-div");
+    html2canvas(element).then((canvas) => {
+      const image = canvas.toDataURL("image/png");
+      const pdf = new jsPDF("p", "mm", "a4");
+      const width = pdf.internal.pageSize.getWidth();
+      const height = (canvas.height * width) / canvas.width;
+
+      pdf.addImage(image, "PNG", 0, 0, width, height);
+      pdf.save("voucher.pdf");
+    });
+  };
+
   return (
     <>
       <div className="absolute inset-0 bg-gray-900 bg-opacity-50 flex items-center h-full justify-center z-50 h-screen overflow-auto">
@@ -13,7 +104,7 @@ const Order = ({ data, onClose }) => {
           <h2 className="text-2xl font-bold mb-4">ORDER</h2>
 
           {/* First Row */}
-          <div className="flex flex-col">
+          <div className="flex sm:flex-row gap-2 flex-col">
             <div className="flex flex-col gap-1">
               <div className="flex flex-col gap-1">
                 <label
@@ -110,13 +201,30 @@ const Order = ({ data, onClose }) => {
                   htmlFor="promoPercentage"
                   className="text-sm decoration-solid capitalize font-bold px-1"
                 >
+                  Qty
+                </label>
+                <input
+                  type="number"
+                  name="qty"
+                  value={orderData.qty}
+                  onChange={handleChange}
+                  className="border-2 border-slate-500 rounded-lg p-1 text-sm sm:text-base placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+                  // placeholder="Enter the sku"
+                  aria-describedby="price-description"
+                />
+              </div>
+              <div className="flex flex-col gap-1">
+                <label
+                  htmlFor="promoPercentage"
+                  className="text-sm decoration-solid capitalize font-bold px-1"
+                >
                   Promo-Percentage
                 </label>
                 <input
                   type="number"
-                  name="price"
-                  value={promoPercentage}
-                  // onChange={handleChange}
+                  name="promoPercentage"
+                  value={orderData.promoPercentage}
+                  onChange={handleChange}
                   className="border-2 border-slate-500 rounded-lg p-1 text-sm sm:text-base placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
                   // placeholder="Enter the sku"
                   aria-describedby="price-description"
@@ -126,7 +234,7 @@ const Order = ({ data, onClose }) => {
               <div className="flex gap-5 w-full shrink justify-start mt-1">
                 <button
                   type="submit"
-                  // onClick={saveStock}
+                  onClick={handleDoneClick}
                   className="px-2 w-20 h-8 bg-blue-500 text-white text-sm rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
                 >
                   {/* {editAble ? "UPDATE" : "ADD"} */}
@@ -141,7 +249,83 @@ const Order = ({ data, onClose }) => {
               </div>
             </div>
             <div>
-              <img src="" alt="" />
+              <div className="flex flex-col items-center space-y-4 p-6">
+                {/* Voucher Div */}
+                <div
+                  id="voucher-div"
+                  className="w-80 p-6 bg-white rounded-lg shadow-md border border-gray-200 "
+                >
+                  <div className="flex flex-row  justify-center items-center">
+                    <h2 className="text-1xl w-full font-bold text-gray-700">
+                      APK <br />T SHIRT
+                    </h2>
+                    <h2 className="text-4xl w-full font-bold text-gray-700">
+                      VOUCHER
+                    </h2>
+                  </div>
+                  <div className="flex flex-col justify-start">
+                    <p className="mt-2 text-sm text-gray-600">
+                      <span className="font-bold text-indigo-500">SKU : </span>
+                      {data.sku}
+                    </p>
+                    <p className="mt-2 text-sm text-gray-600">
+                      <span className="font-bold text-indigo-500">
+                        Product_Name :{" "}
+                      </span>
+                      {data.product_name}
+                    </p>
+                    <p className="mt-2 text-sm text-gray-600">
+                      <span className="font-bold text-indigo-500">
+                        Price :{" "}
+                      </span>
+                      {Number(data.price).toLocaleString()}/Ks
+                    </p>
+                    <p className="mt-2 text-sm text-gray-600">
+                      <span className="font-bold text-indigo-500">Size : </span>
+                      {data.size}
+                    </p>
+                    <p className="mt-2 text-sm text-gray-600">
+                      <span className="font-bold text-indigo-500">
+                        Color :{" "}
+                      </span>
+                      {data.color}
+                    </p>
+                    <p className="mt-2 text-sm text-gray-600">
+                      <span className="font-bold text-indigo-500">Qty : </span>
+                      {orderData.qty || 0}
+                    </p>
+                    <p className="mt-2 text-sm text-gray-600">
+                      <span className="font-bold text-indigo-500">
+                        Total :{" "}
+                      </span>
+                      {Number(orderData.qty * data.price).toLocaleString()}/Ks
+                      {/* Show 0 if qty is undefined */}
+                    </p>
+                    <p className="mt-2 text-sm text-gray-600">
+                      <span className="font-bold text-indigo-500">
+                        PromoPercentage:
+                      </span>
+                      {orderData.promoPercentage}%
+                      {/* Show 0 if qty is undefined */}
+                    </p>
+                    <p className="mt-2 text-sm text-gray-600">
+                      <span className="font-bold text-indigo-500">
+                        Actual Total Price:{" "}
+                      </span>
+                      {(
+                        Number(orderData.qty) *
+                        Number(data.price) *
+                        (1 - Number(orderData.promoPercentage || 0) / 100)
+                      ).toLocaleString()}
+                      /Ks
+                    </p>
+                    <p className="mt-2 text-sm text-gray-600">
+                      Use this code to get 50% off on your next purchase!
+                    </p>
+                  </div>
+                </div>
+              </div>
+
               {/* make me image  and pdf filew */}
             </div>
           </div>
---
+++ src/components/models/ConfirmationModal.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+const ConfirmationModal = ({ isOpen, onClose, onConfirm }) => {
+  if (!isOpen) return null;
+
+  return (
+    <div className="absolute inset-0 bg-gray-900 bg-opacity-50 flex items-center justify-center z-50 h-screen">
+      <div className="bg-white rounded-lg shadow-lg p-6 w-96">
+        <h3 className="text-xl font-bold mb-4">Confirm Action</h3>
+        <p className="text-gray-700 mb-6">Are you sure you want to proceed?</p>
+        <div className="flex justify-end space-x-4">
+          <button
+            onClick={onClose}
+            className="px-4 py-2 bg-gray-500 text-white rounded hover:bg-gray-600"
+          >
+            Cancel
+          </button>
+          <button
+            onClick={onConfirm}
+            className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+          >
+            Confirm
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+
+
+export default Order;
